cbec4f1f1f9d5b07bc433b97906a11813253ecd1
Sharad Vikram <sharad.vikram@gmail.com>
2016-04-11 21:02:14 -0700

    added healths and faint

--- showdown_rl/converter.py
+++ showdown_rl/converter.py
@@ -17,7 +17,7 @@ class Converter(object):
             state, action, _, _ = experience
             for team in [0, 1]:
                 for poke in state.get_team(team):
-                    poke = self.convert_poke_name(poke)
+                    poke = self.convert_poke_name(poke.name)
                     if poke not in self.poke_forward_mapping:
                         self.poke_forward_mapping[poke] = self.poke_index
                         self.poke_backward_mapping.append(poke)
@@ -47,31 +47,38 @@ class Converter(object):
     def convert_poke_name(self, poke):
         return poke.split(',')[0]
 
-    def encode_experience(self, experience):
-        state, action, _, _ = experience
+    def encode_state(self, state):
         poke = state.get_primary(0)
-        poke = self.convert_poke_name(poke)
-        my_team = [self.encode_poke(p) for p in state.get_team(0)]
+        poke = self.convert_poke_name(poke.name)
+        my_team = [self.encode_poke(p.name) for p in state.get_team(0)]
         my_team_vec = [0] * self.poke_index
-        their_team = [self.encode_poke(p) for p in state.get_team(0)]
+        their_team = [self.encode_poke(p.name) for p in state.get_team(0)]
         their_team_vec = [0] * self.poke_index
-        for my_poke, their_poke in zip(my_team, their_team):
+        for my_poke, their_poke in zip(my_team[1:], their_team[1:]):
             my_team_vec[my_poke.argmax()] = 1
             their_team_vec[their_poke.argmax()] = 1
-        x = np.concatenate([self.encode_poke(state.get_primary(0)),
-                            my_team_vec,
-                            self.encode_poke(state.get_primary(1)),
-                            their_team_vec])
+        x = np.concatenate([
+            self.encode_poke(state.get_primary(0).name),
+            my_team_vec,
+            state.get_faints(0) + [1] * (6 - len(my_team)),
+            state.get_healths(0) + [0] * (6 - len(my_team)),
+            self.encode_poke(state.get_primary(1).name),
+            their_team_vec,
+            state.get_faints(1) + [1] * (6 - len(their_team)),
+            state.get_healths(1) + [0] * (6 - len(their_team)),
+        ])
+        return x.astype(np.float32)
 
+    def encode_action(self, action):
         if action.is_move():
             y = self.encode_move(action.name).tolist() + [0] * self.poke_index
         else:
             switch_poke = self.convert_poke_name(action.name)
             y = [0] * self.move_index + self.encode_poke(switch_poke).tolist()
-        return x.astype(np.float32), np.array(y).astype(np.float32)
+        return np.array(y).astype(np.float32)
 
     def get_input_dimension(self):
-        return self.poke_index * 4
+        return self.poke_index * 4 + 24
 
     def get_output_dimension(self):
         return self.move_index + self.poke_index
--- train_model.py
+++ train_model.py
@@ -6,13 +6,25 @@ from tqdm import tqdm
 from showdown_parser import ReplayDatabase, parse_log
 from showdown_rl import Converter
 
-def predict(experience):
+def correct_poke(poke):
+    return poke.split(',')[0]
 
-    x, _ = converter.encode_experience(experience)
+def predict(experience):
+    state, action, _, _ = experience
+
+    x = converter.encode_state(state)
+
     probs = net.predict(x[None])[0]
     predictions = probs.argsort()[::-1][:5]
 
-    print experience[0], experience[1]
+    print "Matchup: %s[%.2f] vs %s[%.2f]" % (correct_poke(state.get_primary(0).name),
+                                         state.get_health(0),
+                                         correct_poke(state.get_primary(1).name),
+                                         state.get_health(1))
+    print "My team: %s" % ', '.join(["%s[%f]" % (correct_poke(p.get_name()), p.health) for p in state.get_team(0)[1:]])
+    print "Their team: %s" % ', '.join([correct_poke(p.get_name()) for p in state.get_team(1)[1:]])
+    print
+    print "My action: %s" % experience[1]
     print
 
     for i, prediction in enumerate(predictions):
@@ -28,9 +40,11 @@ def train(iters):
 
     idx = np.arange(len(experiences))
     avg_loss = None
+    exs = np.random.permutation(experiences)
     for i in xrange(iters):
-        es = experiences[np.random.permutation(idx)[:200]]
-        X, y = zip(*[converter.encode_experience(e) for e in es])
+        ix = np.random.choice(idx)
+        es = exs[ix: ix + 512]
+        X, y = zip(*[(converter.encode_state(e[0]), converter.encode_action(e[1])) for e in es])
         X = np.stack(X, axis=0)
         y = np.stack(y, axis=0)
         loss = adam.train(X, y, 0.001)
@@ -41,20 +55,20 @@ def train(iters):
         print "Loss[%u]: %.3f [%.3f]" % (i, loss, avg_loss)
 
 if __name__ == "__main__":
-    #r = ReplayDatabase('replays_all.db')
-    #experiences = []
-    #for id, replay_id, log in tqdm(r.get_replays(limit=10000)):
-        #try:
-            #for experience in parse_log(log):
-                #experiences.append(experience)
-        #except:
-            #pass
-    #converter = Converter()
-    #converter.learn_encodings(experiences)
-    #experiences = np.array(experiences)
-    ##np.random.seed(100)
-    ##experiences = np.random.permutation(experiences)[:10000]
+    r = ReplayDatabase('replays_all.db')
+    experiences = []
+    for id, replay_id, log in tqdm(r.get_replays(limit=50000)):
+        try:
+            for experience in parse_log(log):
+                experiences.append(experience)
+        except:
+            print "Failed on replay:", replay_id
+            # import traceback
+            # traceback.print_exc()
+    converter = Converter()
+    converter.learn_encodings(experiences)
+    experiences = np.array(experiences)
 
-    #net = Vector(converter.get_input_dimension()) >> Repeat(Tanh(1500), 2) >> Softmax(converter.get_output_dimension())
+    net = Vector(converter.get_input_dimension()) >> Repeat(Tanh(1000), 2) >> Softmax(converter.get_output_dimension())
 
     adam = Adam(net >> CrossEntropy())
